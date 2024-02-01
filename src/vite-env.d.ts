9f4e4c8db10f437d98c3038fae15b4ba82e5f623
kduyyyy1levinci <122582194+kduyyyy1levinci@users.noreply.github.com>
2024-02-01 22:36:54 +0700

    Config env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_REACT_APP_STATUS: string;
+  readonly VITE_REACT_APP_URL_API: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,12 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [
-      { find: '@', replacement: '/src' },
-      { find: 'assets', replacement: '/src/assets' },
-      { find: 'components', replacement: '/src/components' },
-      { find: 'utils', replacement: '/src/utils' },
-    ],
+    alias: {
+      '@': '/src',
+      assets: '/src/assets',
+      components: '/src/components',
+      utils: '/src/utils',
+      '/src/main.tsx': '/src/index.tsx',
+    },
   },
 });
